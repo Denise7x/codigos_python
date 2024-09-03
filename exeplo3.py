d6dea5da4eab118a465464088de2d811f5776075
Denise Ferreira <denisesampaioferreira@gmail.com>
2024-09-03 15:25:23 -0300

    Trabalhando com input em python

---
+++ exeplo3.py
@@ -0,0 +1,7 @@
+nota1 =float( input("Informe a 1 nota: ")) #estamos conevretendo valores textuais em valores decimais
+nota2 = float(input("Informe a 2 nota: "))
+nota3 = float(input("Informe a 3 nota: "))
+
+media = (nota1+nota2+nota3)/3
+
+print(f"sua media é {media:.2f}") #.2f = duas caasas decimais apos a virgula
