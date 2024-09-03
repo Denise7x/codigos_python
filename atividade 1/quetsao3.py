436c8317fa45f1387c47b5d69c9648fa36da3b83
Denise Ferreira <denisesampaioferreira@gmail.com>
2024-09-03 16:50:52 -0300

    Atividade questões 1 e 3

---
+++ atividade 1/quetsao3.py
@@ -0,0 +1,5 @@
+nomeF= input("Informe nome da fazenda: ")
+qtdC= int(input("Informe quantidade de cavalos que vivem nessa fazenda: "))
+ferraduras = qtdC*4
+
+print(f"A fazenda {nomeF} possui {qtdC} cavalos,  a quatidade de ferraduras para equipar todos os cavalos é de: {ferraduras}")
